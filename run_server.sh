652d3897272d37acbeade251d581f874b13fe8e5
Paul Wilson <pfrwilson@gmail.com>
2021-11-16 19:53:49 -0500

    easy to run scripts

---
+++ run_server.sh
@@ -0,0 +1,9 @@
+if [ -d ./venv ]; then
+python -m venv ./venv
+source venv/bin/activate
+pip install -r requirements.txt
+else
+source venv/bin/activate
+fi 
+export PYTHONPATH=$(pwd)
+python server/servermain.py
